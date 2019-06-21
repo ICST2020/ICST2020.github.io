7db5b8dfb3ed14f0b3e0c488331ab8e411e2e99c
Luis Cruz <luismirandacruz@gmail.com>
2019-06-21 15:38:14 +0100

    vmware

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/sh
 
 REQUEST_URL="https://docs.google.com/spreadsheets/d/1J3EB2gT1MvPYNKBSLi9zLDHAAZYrX5Hut6Lawy8a2dg/export?format=csv&id=1J3EB2gT1MvPYNKBSLi9zLDHAAZYrX5Hut6Lawy8a2dg&gid=0"
-REQUEST_URL="https://docs.google.com/spreadsheets/d/1J3EB2gT1MvPYNKBSLi9zLDHAAZYrX5Hut6Lawy8a2dg/gviz/tq?tqx=out:csv&sheet=Committee"
+# REQUEST_URL="https://docs.google.com/spreadsheets/d/1J3EB2gT1MvPYNKBSLi9zLDHAAZYrX5Hut6Lawy8a2dg/gviz/tq?tqx=out:csv&sheet=Committee"
 
 curl $REQUEST_URL > _data/organizing_committee.csv
